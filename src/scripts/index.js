2bbdbed1faccb90c6e4d274fd89515c2581766f8
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2021-04-29 18:59:07 +0800

    Create entry point.

---
+++ src/scripts/index.js
@@ -0,0 +1,36 @@
+import 'regenerator-runtime';
+import '../styles/style.scss';
+
+import './components/app-bar';
+import './components/hero-element';
+import './components/resto-list';
+import './components/footer-app';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.querySelector("#menu-btn");
+    const body = document.querySelector("body");
+    const header = document.querySelector(".app-bar");
+
+    let isMenuOpen = false;
+
+    const headerBackgroundEvent = () => {
+        window.pageYOffset > 320
+            ? header.classList.add("background")
+            : header.classList.remove("background");
+    };
+
+    headerBackgroundEvent();
+
+    menuBtn.addEventListener("click", event => {
+        isMenuOpen = !isMenuOpen;
+        menuBtn.textContent = isMenuOpen ? "⨉" : "☰";
+        header.classList.toggle("menu__open");
+        body.classList.toggle("hideoverflow");
+        event.stopPropagation();
+    });
+
+    window.addEventListener('scroll', () => {
+        headerBackgroundEvent();
+    });
+});
