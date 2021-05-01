6ad2c4984dc9de93447b19a8e501d415f1b14b7d
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2021-05-02 06:58:05 +0800

    Add FaviconsWebpackPlugin.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -37,5 +38,9 @@ module.exports = merge(common, {
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({filename: 'assets/css/[name].css'}),
+    new FaviconsWebpackPlugin({
+      logo: './src/public/images/logo/logo.svg',
+      prefix: 'assets/icons/',
+    }),
   ],
 });
