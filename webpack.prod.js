3443d9e7f9dc4f273d31730c07d6c7ca24f31094
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2021-06-15 21:48:28 +0800

    Add html-webpack-inject-preload.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,27 @@
+/* eslint-disable max-len */
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackInjectPreload = require('@principalstudio/html-webpack-inject-preload');
 
 module.exports = merge(common, {
   mode: 'production',
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: '/node_modules/',
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        ],
-      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: '/node_modules/',
+      //   use: [
+      //     {
+      //       loader: 'babel-loader',
+      //       options: {
+      //         presets: ['@babel/preset-env'],
+      //       },
+      //     },
+      //   ],
+      // },
       {
         test: /\.s[ac]ss$/i,
         use: [
@@ -35,7 +38,16 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({filename: 'assets/css/[name][hash].css'}),
+    // new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({filename: 'assets/css/[hash].css'}),
+    new HtmlWebpackInjectPreload({
+      files: [
+        {
+          match: /.*\.css$/,
+          attributes: {as: 'style', type: 'text/css'},
+        },
+      ],
+    }),
   ],
   optimization: {
     minimizer: [
