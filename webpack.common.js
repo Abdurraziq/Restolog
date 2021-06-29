e0bb038b6c686cd02497fd04827910fbc9244e32
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2021-06-29 13:44:41 +0800

    Reconfig some loader and plugin.

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const {InjectManifest} = require('workbox-webpack-plugin');
-
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'assets/js/[hash].js',
+    filename: 'assets/js/[name].js',
     assetModuleFilename: 'assets/[hash][ext]',
     clean: true,
   },
@@ -43,36 +40,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/templates/index.html'),
       favicon: path.resolve(__dirname, 'src/public/images/icons/favicon.ico'),
-    }),
-    new WebpackPwaManifest({
-      name: 'Restolog',
-      short_name: 'Restolog',
-      description: 'A restaurant catalog application',
-      start_url: './index.html',
-      background_color: '#FFFFFF',
-      theme_color: '#252A34',
-      display: 'standalone',
-      orientation: 'any',
-      publicPath: './',
-      filename: 'site.webmanifest',
-      ios: true,
-      icons: [
-        {
-          src: path.resolve(__dirname, 'src/public/images/icons/logo.png'),
-          size: 180,
-          destination: 'assets/icons',
-          ios: true,
-        },
-        {
-          src: path.resolve(__dirname, 'src/public/images/icons/logo.png'),
-          sizes: [72, 96, 128, 192, 256, 384, 512],
-          destination: 'assets/icons',
-          purpose: 'any maskable',
-        },
-      ],
-    }),
-    new InjectManifest({
-      swSrc: './src/scripts/sw.js',
     }),
   ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,23 +5,26 @@ const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackInjectPreload = require('@principalstudio/html-webpack-inject-preload');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const {InjectManifest} = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: '/node_modules/',
-      //   use: [
-      //     {
-      //       loader: 'babel-loader',
-      //       options: {
-      //         presets: ['@babel/preset-env'],
-      //       },
-      //     },
-      //   ],
-      // },
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
       {
         test: /\.s[ac]ss$/i,
         use: [
@@ -38,7 +41,7 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    // new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin(),
     new MiniCssExtractPlugin({filename: 'assets/css/[hash].css'}),
     new HtmlWebpackInjectPreload({
       files: [
@@ -47,6 +50,36 @@ module.exports = merge(common, {
           attributes: {as: 'style', type: 'text/css'},
         },
       ],
+    }),
+    new WebpackPwaManifest({
+      name: 'Restolog',
+      short_name: 'Restolog',
+      description: 'A restaurant catalog application',
+      start_url: './index.html',
+      background_color: '#FFFFFF',
+      theme_color: '#252A34',
+      display: 'standalone',
+      orientation: 'any',
+      publicPath: './',
+      filename: 'site.webmanifest',
+      ios: true,
+      icons: [
+        {
+          src: path.resolve(__dirname, 'src/public/images/icons/logo.png'),
+          size: 180,
+          destination: 'assets/icons',
+          ios: true,
+        },
+        {
+          src: path.resolve(__dirname, 'src/public/images/icons/logo.png'),
+          sizes: [72, 96, 128, 192, 256, 384, 512],
+          destination: 'assets/icons',
+          purpose: 'any maskable',
+        },
+      ],
+    }),
+    new InjectManifest({
+      swSrc: './src/scripts/sw.js',
     }),
   ],
   optimization: {
@@ -65,5 +98,26 @@ module.exports = merge(common, {
         },
       }),
     ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
 });
