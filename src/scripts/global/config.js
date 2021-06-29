f5ab7bbd23dddd611b8f8554d665b1bc8b1a6107
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2021-06-29 13:50:16 +0800

    Rename.

---
+++ src/scripts/global/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  AUTH_TOKEN: 12345,
+  API_BASE_URL: 'https://restaurant-api.dicoding.dev',
+  SMALL_BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
+  MEDIUM_BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  PRECACHE_PREFIX: 'restolog',
+  PRECACHE_SUFFIX: 'v1',
+  PRECACHE_NAME: 'precache',
+  API_CACHE_NAME: 'api-cache',
+  IMAGE_CACHE_NAME: 'img-cache',
+  DATABASE_NAME: 'restolog-idb',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant',
+};
+
+export default CONFIG;
